a7f1986aae9e9e089c059ac19a50b0b5c2fd48c4
soarevanessa <soarevanessaluiza@gmail.com>
2026-05-01 14:10:10 +0300

    Add files via upload

---
+++ date.py
@@ -0,0 +1,58 @@
+MENIU = {
+    "1": {
+        "nume": "Espresso",
+        "pret": 10,
+        "ingrediente": {
+            "cafea_g": 18,
+            "apa_ml": 50
+        }
+    },
+    "2": {
+        "nume": "Cappuccino",
+        "pret": 14,
+        "ingrediente": {
+            "cafea_g": 18,
+            "lapte_ml": 100,
+            "apa_ml": 50
+        }
+    },
+    "3": {
+        "nume": "Latte Machiato",
+        "pret": 16,
+        "ingrediente": {
+            "cafea_g": 18,
+            "lapte_ml": 200,
+            "apa_ml": 50
+        }
+    },
+    "4": {
+        "nume": "Ciocolata calda",
+        "pret": 12,
+        "ingrediente": {
+            "pudra_cacao_g": 20,
+            "lapte_ml": 250
+        }
+    },
+    "5": {
+        "nume": "Ceai",
+        "pret": 8,
+        "ingrediente": {
+            "apa_ml": 250,
+            "pliculet_ceai_buc": 1
+        }
+    }
+}
+NUME_INGREDIENTE = {
+    "cafea_g": "Cafea",
+    "apa_ml": "Apa",
+    "lapte_ml": "Lapte",
+    "pudra_cacao_g": "Pudra cacao",
+    "pliculet_ceai_buc": "Pliculete ceai"
+}
+UNITATI = {
+    "cafea_g": "g",
+    "apa_ml": "ml",
+    "lapte_ml": "ml",
+    "pudra_cacao_g": "g",
+    "pliculet_ceai_buc": "buc"
+}
